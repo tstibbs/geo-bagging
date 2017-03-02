a9da57da7575aacaa13396b089941684a21602e1
tstibbs <tstibbs@users.noreply.github.com>
2017-03-02 20:58:51 +0000

    Showing image links on defence data

--- js/bundles/defence/points_builder.js
+++ js/bundles/defence/points_builder.js
@@ -2,7 +2,7 @@ define(['../abstract_points_builder'],
 	function(AbstractPointsBuilder) {
 		return AbstractPointsBuilder.extend({
 			parse: function(point) {
-				//[Longitude,Latitude,Id,Name,location,condition,description]
+				//[Longitude,Latitude,Id,Name,location,condition,description,imageLinks]
 				var lng = point[0];
 				var lat = point[1];
 				var id = point[2];
@@ -11,11 +11,13 @@ define(['../abstract_points_builder'],
 				var location = point[5];
 				var condition = point[6];
 				var description = point[7];
+				var imageLinks = point[8];
 				
 				var extraInfos = {
 					'Style': name,
 					'Position': location,
-					'Notes': description
+					'Notes': description,
+					'Images': this.buildImageLinks(imageLinks)
 				};
 				this.addMarker(id, lat, lng, url, id, extraInfos, null, [condition]);
 			},
