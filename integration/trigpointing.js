287b1248e6744edcea0083ac8748382b66c27163
tstibbs <tstibbs@users.noreply.github.com>
2018-03-27 21:11:54 +0100

    Loading nearby trigs from search results instead of pre-cached trig data

--- integration/trigpointing.js
+++ integration/trigpointing.js
@@ -1,41 +1,11 @@
 //$.getScript("https://tstibbs.github.io/geo-bagging/integration/trigpointing.js");
 
-$.get('down-flash.php' + window.location.search, function(data) {
-	var regex = /Showing trigpoints \d+ to \d+ of (\d+)/g;
-	var allText = $('body').text();
-	
-	var showMap;
-	if (regex.test(allText) && parseInt(regex.exec(allText)[1]) > 1000) {
-		showMap = confirm("Map will only display the first 1000 points, are you sure you want to continue?");
-	} else {
-		showMap = true;
-	}
+var urlBase = 'https://tstibbs.github.io/geo-bagging/';
 
-	if (showMap === true) {
-		var decodedXmlString = decodeURIComponent(data);
-		var dom = $($.parseXML(decodedXmlString));
-		var points = dom.find('C').map(function(i, element) {
-			//e.g. <C D='Castlebythe Barrow' I='2041' E='202873' N='229647' F='n'/>
-			var point = $(element);
-			var url = 'http://trigpointing.uk/trig/' + point.attr('I');
-			return {
-				eastings: point.attr('E'),
-				northings: point.attr('N'),
-				url: url,
-				name: point.attr('D')
-			};
-		}).toArray();
-		
-		var options = {
-			pointsToLoad: points
-		};
-
-		var urlBase = 'https://tstibbs.github.io/geo-bagging/';
-		$.getScript(urlBase + "js/loader.js").then(function() {
-			loadApp(urlBase, function(main) {
-				$('body').empty();//our map assumes it is full screen - there's probably a better way, but this will work for now
-				main.loadMap(options);
-			});
+$.getScript(urlBase + "js/loader.js").then(function() {
+	loadApp(urlBase, function(main) {
+		require(['integration/tuk'], function(tukIntegration) {
+			tukIntegration.showMapForSearch();
 		});
-	}
+	});
 });
