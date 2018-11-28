a0b328c66ceeebfc33563e463ba087c63925a07d
tstibbs <tstibbs@users.noreply.github.com>
2018-11-28 21:42:48 +0000

    Option to constrain markers to local area

--- js/menu/constraints_view.js
+++ js/menu/constraints_view.js
@@ -1,33 +1,76 @@
 define(['jquery', 'leaflet'],
 	function($, leaflet) {
 		return leaflet.Class.extend({
-			initialize: function(manager, map) {
+			initialize: function(manager) {
 				this._manager = manager;
 				this._view = $('<div></div>');
 				var info = $('<div class="info">This section allows you to limit how much data is displayed on screen. This can be useful for example when on older or mobile devices which may struggle to remain responsive if a large amount of data is loaded.</div>')
 				this._view.append(info);
 				this._view.append('<hr class="info">');
-				var currentAreaLimit = $('<input type="checkbox">');
 				
+				var currentAreaView = this._buildCurrentAreaView();
+				this._view.append(currentAreaView);
+				
+				var currentLocationView = this._buildCurrentLocationView();
+				this._view.append(currentLocationView);
+			},
+			
+			_buildCurrentAreaView: function() {
+				var wrapper = $('<div class="setting"></div>');
+				this._currentAreaLimit = $('<input type="checkbox">');
 				var desc = $('<label>Limit new markers to current area</label>');
-				desc.prepend(currentAreaLimit);
-				this._view.append(desc);
+				desc.prepend(this._currentAreaLimit);
 				
-				currentAreaLimit.change(function(event) {
+				this._currentAreaLimit.change(function(event) {
 					if (event.target.checked) {
 						this._limitToCurrentView();
 					} else {
 						this._unlimit();
 					}
 				}.bind(this));
+				
+				wrapper.append(desc);
+				return wrapper;
+			},
+			
+			_buildCurrentLocationView: function() {
+				var wrapper = $('<div class="setting"></div>');
+				var currentLocationLimitButton = $('<button type="button">Limit to 5 miles from current location</button>');
+				currentLocationLimitButton.click(this._limitToCurrentLocation.bind(this));
+				wrapper.append(currentLocationLimitButton);
+				return wrapper;
+			},
+			
+			_limitToCurrentLocation: function() {
+				var map = this._manager.getMap();
+				var listener = function(e) {
+					map.off('locationfound', listener);//only want to be called once
+					var lat = e.latlng.lat;
+					var lng = e.latlng.lng;
+					var latExtra = 0.08;//roughly 5 miles difference in lattitude in the UK
+					var lngExtra = 0.12;//roughly 5 miles difference in longitude in the UK
+					var latLngBounds = new leaflet.latLngBounds(
+						[lat + latExtra, lng + lngExtra],
+						[lat - latExtra, lng - lngExtra]
+					);
+					this._limitTo(latLngBounds);
+				}.bind(this);
+				map.on('locationfound', listener);
+				map.stopLocate();
+				map.locate();
 			},
 			
 			_limitToCurrentView: function() {
-				this._manager.setViewConstraints(this._manager.getMap().getBounds());
+				this._limitTo(this._manager.getMap().getBounds());
+			},
+			
+			_limitTo: function(bounds) {
+				this._manager.setViewConstraints(bounds);
 			},
 			
 			_unlimit: function() {
 				this._manager.setViewConstraints(null);
+				this._currentAreaLimit.prop('checked', false);
 			},
 			
 			getView: function() {
