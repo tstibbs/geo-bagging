11c7ff83bf0ed7bda11d8c985d5646b3c37c73e4
tstibbs <tstibbs@users.noreply.github.com>
2020-05-26 19:13:21 +0000

    Adding categories to defence

--- converters/defence.js
+++ converters/defence.js
@@ -5,7 +5,7 @@ const Converter = require('./converter');
 const xslt = require('./xslt');
 
 const attributionString = "This file adapted from the database of the Defence of Britain project (http://archaeologydataservice.ac.uk/archives/view/dob/download.cfm). Copyright of the Council for British Archaeology (2006) Defence of Britain Archive [data-set]. York: Archaeology Data Service [distributor] https://doi.org/10.5284/1000327";
-const columnHeaders = "[Longitude,Latitude,Id,Name,Link,location,condition,description,imageLinks]"
+const columnHeaders = "[Longitude,Latitude,Id,Categories,Link,location,condition,description,imageLinks]"
 
 class DobConverter extends Converter {
 	constructor() {
@@ -29,18 +29,47 @@ class DobConverter extends Converter {
 		let coords = point.coordinates[0].split(',');
 		let lng = coords[0];
 		let lat = coords[1];
-		let name = point.name[0];
+        let name = point.name[0];
 		let id = '';
-		if (name.includes(': ')) {
-			let splits = name.split(': ');
-			name = splits.slice(0, splits.length - 1).join(': ');//all but the last bit
-			id = splits[splits.length - 1];
-		}
+        if (name.includes(': ')) {
+			id = name.split(': ').slice(-1)[0];
+		} else {
+            console.error(`No id found, name was: ${name}`)
+        }
+        let folders = point.folders[0].folder
+        if (condition == "Removed" || condition == "Infilled") {
+            let last = folders.slice(-1)[0]
+            if (last.startsWith("Removed ")) {
+                let penultimate = folders.slice(-2)[0]
+                let lastWithoutRemoved = last.substring("Removed ".length)
+                if (penultimate == lastWithoutRemoved) {
+                    //the last one is just a duplicate of the penulatimate one, but with 'removed' at the start, but this is redundant due to the condition field anyway
+                    folders.splice(-1)//i.e. drop the last one
+                } else {
+                    //last one is not a dup, so just tidy up to remove the redundant 'removed ' part
+                    folders[folders.length - 1] = lastWithoutRemoved
+                }
+            }
+        }
+        folders = folders
+            .filter(folder => folder != 'Defence of Britain Sites')
+            .map(folder => folder.endsWith(':') ? folder.slice(0, -1) : folder)//TODO if this matches the 'style' then remove it?
+            .map(folder => folder.replace(/ 0F /g, ' of '))//fixing a weird typo
+            .map(folder =>
+                //convert to title case
+                folder.replace(/\b[a-zA-Z]+\b/g, part => //only touch things that look like words
+                    part.slice(0, 1).toUpperCase() + part.slice(1).toLowerCase()
+                    //part.toLowerCase().replace(/(^| )\w/g, char => char.toUpperCase())
+                )
+                .replace(' Of ', ' of ')
+                .replace(' And ', ' and ')
+            )
+
 		return [
 			lng,
 			lat,
 			id,
-			name,
+			folders,
 			link,
 			location,
 			condition,
--- js/bundles/defence/points_builder.js
+++ js/bundles/defence/points_builder.js
@@ -2,22 +2,24 @@ define(['../abstract_points_builder'],
 	function(AbstractPointsBuilder) {
 		return AbstractPointsBuilder.extend({
 			parse: function(point) {
-				//[Longitude,Latitude,Id,Name,location,condition,description,imageLinks]
+				//[Longitude,Latitude,Id,Categories,location,condition,description,imageLinks]
 				var lng = point[0];
 				var lat = point[1];
 				var id = point[2];
-				var name = point[3];
+				var categories = point[3];
 				var url = point[4];
 				var location = point[5];
 				var condition = point[6];
 				var description = point[7];
 				var imageLinks = point[8];
-				
+                
+                var style = categories.slice(-1)[0]
 				var extraInfos = {
-					'Style': name,
+					'Style': style,
 					'Position': location,
 					'Notes': description,
-					'Images': this.buildImageLinks(imageLinks)
+                    'Images': this.buildImageLinks(imageLinks),
+                    'Categories': categories.slice(0, -1)
 				};
 				this.addMarker(id, lat, lng, url, id, extraInfos, null, [condition]);
 			},
