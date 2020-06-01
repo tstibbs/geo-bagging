bc7afefc90c11785779ade858a08a3f5cbae15e8
tstibbs <tstibbs@users.noreply.github.com>
2020-06-01 19:50:52 +0000

    Upgrading leaflet to fix mobile issues + related library upgrades (as as we were upgrading the bing layer, fixing that up to reference latest layer types)

--- js/app.js
+++ js/app.js
@@ -4,30 +4,30 @@ if (window != null && window.os_map_base !== undefined) {
 }
 
 var versions = {
-	leaflet: '1.3.4',
-	leaflet_bing: '3.0.3',
-    proj4: '2.3.14',
+	leaflet: '1.6.0',
+	leaflet_bing: '3.3.1',
+    proj4: '2.6.2',
 	leaflet_cluster: '1.4.1',
 	leaflet_mouseposition: 'b628c7be754c134c63117b3feb75e720a1d20673',
 	leaflet_screenposition: '614445ce5e06b1ce20f78b08aad0b9f6e64d6c37',
 	leaflet_subgroup: '1.0.2',
 	leaflet_matrixlayers: 'c8f0758da314e6eb0716d615b035347a547175ff',
-	leaflet_locate: '0.63.0',
+	leaflet_locate: '0.72.0',
 	leaflet_controlHider: '3df76ebbfda70789027a40aa6f2e05db603aa364',
 	leaflet_boxSelector: '11007fa9e2553353a53c7523d5d964aa8c553fe5',
 	leaflet_geosearch: 'ce8da4ded7abbf7c1f590a3337a70e7e25146383',
 	leaflet_sidebar: '0.4.0',
 	toGeoJson: 'v0.16.0',
-	file_saver: '1.3.8',
-	underscore: '1.9.1',
-	jquery: '3.3.1',
+	file_saver: '2.0.2',
+	underscore: '1.10.2',
+	jquery: '3.5.1',
 	Squire: '0.2.1',
 	sinon: '1.17.5',
 	font_awesome: '4.5.0'
 };
 var packages = {
 	leaflet_bing: 'leaflet-plugins',
-	proj4: 'proj4js',
+	proj4: 'proj4',
 	leaflet_cluster: 'leaflet.markercluster',
 	leaflet_mouseposition: 'tstibbs/Leaflet.MousePosition',
 	leaflet_screenposition: 'xguaita/Leaflet.MapCenterCoord',
@@ -41,7 +41,7 @@ var packages = {
 	toGeoJson: '@mapbox/togeojson',
 	file_saver: 'file-saver',
 	font_awesome: 'font-awesome',
-	underscore: 'underscore.js',
+	underscore: 'underscore',
 	Squire: 'squirejs',
 };
 
@@ -51,17 +51,14 @@ function pkg(package) {
 function unpkg(package, path) {
 	return 'https://unpkg.com/' + pkg(package) + '@' + versions[package] + '/' + path;
 }
-function cloudflare(package, path) {
-	return 'https://cdnjs.cloudflare.com/ajax/libs/' + pkg(package) + '/' + versions[package] + '/' + path;
-}
 function jsdelivrGh(package, path) {
 	return 'https://cdn.jsdelivr.net/gh/' + pkg(package) + '@' + versions[package] + '/' + path;
 }
 
 var paths = {
 	leaflet: unpkg('leaflet', 'dist/leaflet'),
-	leaflet_bing: cloudflare('leaflet_bing', 'layer/tile/Bing'),
-	proj4: cloudflare('proj4', 'proj4'),
+	leaflet_bing: unpkg('leaflet_bing', 'layer/tile/Bing'),
+	proj4: unpkg('proj4', 'dist/proj4'),
 	leaflet_cluster: unpkg('leaflet_cluster', 'dist/leaflet.markercluster-src'),
 	leaflet_mouseposition: jsdelivrGh('leaflet_mouseposition', 'src/L.Control.MousePosition'),
 	leaflet_screenposition: jsdelivrGh('leaflet_screenposition', 'src/L.Control.MapCenterCoord'),
@@ -75,8 +72,8 @@ var paths = {
 	leaflet_geosearch_bing: jsdelivrGh('leaflet_geosearch', 'src/js/l.geosearch.provider.bing'),
 	leaflet_sidebar: unpkg('leaflet_sidebar', 'js/leaflet-sidebar'),
 	toGeoJson: unpkg('toGeoJson', 'togeojson'), 
-	file_saver: unpkg('file_saver', 'FileSaver.min'),
-	underscore: cloudflare('underscore', 'underscore-min'),
+	file_saver: unpkg('file_saver', 'src/FileSaver'),
+	underscore: unpkg('underscore', 'underscore-min'),
 	jquery: 'https://code.jquery.com/jquery-' + versions.jquery
 };
 
