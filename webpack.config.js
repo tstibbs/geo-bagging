67130c54d182f0ef5d4dd88d06e479816aa79cc3
tstibbs <tstibbs@users.noreply.github.com>
2020-06-13 20:45:44 +0100

    webpack: enabling css

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,33 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'GeoBagging'
-        })
+    module: {
+        rules: [
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
     ]
+}
 }
