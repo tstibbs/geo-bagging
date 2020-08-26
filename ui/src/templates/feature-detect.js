9b8cd99091f02d097f17303d1456d367cf32a3a8
Tim Stibbs <tstibbs@users.noreply.github.com>
2020-08-26 21:09:23 +0100

    webpack: adding more feature checks for things that fail in IE

--- ui/src/templates/feature-detect.js
+++ ui/src/templates/feature-detect.js
@@ -16,6 +16,7 @@ test("'a'.includes('b')", 'String.prototype.includes');
 test("['a'].includes('b')", 'Array.prototype.includes');
 test("['a'].map(elem => elem + 'b')", 'arrow functions');
 test("document.createElement('div').remove()", 'ChildNode.remove');
+test("var a = document.currentScript.src", 'document.currentScript')
 
 if (unsupportedFeatures.length > 0) {
     var errorMessage = "Browser does not support some features required by geo-bagging (" + unsupportedFeatures.join(', ') + "), please either use a more modern browser (ideally Chrome) or report a bug at https://github.com/tstibbs/geo-bagging/issues/new.";
