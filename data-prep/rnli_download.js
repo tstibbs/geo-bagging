6efe0e634754bb9e360071c4563bca6a01a11fe0
Tim Stibbs <tstibbs@users.noreply.github.com>
2020-09-02 20:19:22 +0100

    Fixed incorrect error log in rnli source

--- data-prep/rnli_download.js
+++ data-prep/rnli_download.js
@@ -6,8 +6,7 @@ const inputDir = `${tmpInputDir}/rnli`
 
 function downloadStationsList() {
 	const urls = {
-		'https://opendata.arcgis.com/datasets/7dad2e58254345c08dfde737ec348166_0.csv':
-			'lifeboatStations.csv' //https://hub.arcgis.com/datasets/7dad2e58254345c08dfde737ec348166_0
+		'https://opendata.arcgis.com/datasets/7dad2e58254345c08dfde737ec348166_0.csv': 'lifeboatStations.csv' //https://hub.arcgis.com/datasets/7dad2e58254345c08dfde737ec348166_0
 	}
 	return downloadFiles('rnli', urls)
 }
@@ -18,10 +17,10 @@ async function downloadEnrichingData() {
 	await writeFile(`${inputDir}/wiki.json`, dataString)
 }
 
-function download() {
+async function download() {
 	let p1 = downloadStationsList()
 	let p2 = downloadEnrichingData()
-	return Promise.all([p1, p2])
+	await Promise.all([p1, p2])
 }
 
 ifCmd(import.meta, download)
