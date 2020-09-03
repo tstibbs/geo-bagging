c68a77706652c99fe45eeeb62735053c10fd6a45
Tim Stibbs <tstibbs@users.noreply.github.com>
2020-09-03 21:32:50 +0100

    Adding copyright statement to UI

--- ui/src/js/menu/attribution.js
+++ ui/src/js/menu/attribution.js
@@ -4,13 +4,29 @@ import leaflet from 'VendorWrappers/leaflet'
 var AttributionView = leaflet.Class.extend({
 	initialize: function (container) {
 		this._container = container
+		let copyright = $(
+			`<div class="attribution-entry"></div>
+	<span class="attribution-source">Geo-bagging</span>
+	<span>
+		The code on this site is copyright 
+		<a href="https://github.com/tstibbs">tstibbs</a>/stripycoder, 
+		licenced under AGPL, 
+		source code available at 
+		<a href="https://github.com/tstibbs/geo-bagging">here</a>.
+	</span>
+</div>`
+		)
+		this._container.append(copyright)
 	},
 
 	addAttribution: function (dataSourceName, text) {
-		var wrapper = $('<div class="attribution-entry"></div>')
-		wrapper.append($('<span class="attribution-source"></span>').text(dataSourceName))
-		wrapper.append($('<span></span>').html(text))
-		this._container.append(wrapper)
+		let attribution = $(
+			`<div class="attribution-entry">
+	<span class="attribution-source">${dataSourceName} data</span>
+	<span>${text}</span>
+</div>`
+		)
+		this._container.append(attribution)
 	}
 })
 
