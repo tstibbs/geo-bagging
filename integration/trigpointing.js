6a241174443549ce2fe5220787d05debe4ba6f77
Tim Stibbs <tstibbs@users.noreply.github.com>
2020-09-07 20:56:26 +0000

    Deploying 27754fe5d553c12691bb0ee3a3b9fd36752350e8

--- integration/trigpointing.js
+++ integration/trigpointing.js
@@ -1,40 +1,43 @@
 function showError(errorMessage) {
     alert(errorMessage)
 }
-var unsupportedFeatures = [];
+var unsupportedFeatures = []
 
 function test(script, name) {
-    try {
-        eval(script);
-    } catch (err) {
-        unsupportedFeatures.push(name);
-        console.error(err);
-    }
+	try {
+		eval(script)
+	} catch (err) {
+		unsupportedFeatures.push(name)
+		console.error(err)
+	}
 }
 
-test("let val = 'a'", 'let');
-test("const arr = 'a'", 'const');
-test("var str = `abc`", 'backticks');
-test("'a'.includes('b')", 'String.prototype.includes');
-test("['a'].includes('b')", 'Array.prototype.includes');
-test("['a'].map(elem => elem + 'b')", 'arrow functions');
-test("document.createElement('div').remove()", 'ChildNode.remove');
-test("var a = document.currentScript.src", 'document.currentScript')
+test("let val = 'a'", 'let')
+test("const arr = 'a'", 'const')
+test('var str = `abc`', 'backticks')
+test("'a'.includes('b')", 'String.prototype.includes')
+test("['a'].includes('b')", 'Array.prototype.includes')
+test("['a'].map(elem => elem + 'b')", 'arrow functions')
+test("document.createElement('div').remove()", 'ChildNode.remove')
+test('var a = document.currentScript.src', 'document.currentScript')
 
 if (unsupportedFeatures.length > 0) {
-    var errorMessage = "Browser does not support some features required by geo-bagging (" + unsupportedFeatures.join(', ') + "), please either use a more modern browser (ideally Chrome) or report a bug at https://github.com/tstibbs/geo-bagging/issues/new.";
-    try {
-        console.log(errorMessage);
-        console.log(typeof showError);
-        if (typeof showError === 'function') {
-            showError(errorMessage);
-        }
-        document.execCommand('Stop');
-    } catch (err) {
-        console.error(err);
-        //catch but just log so we can continue to try to throw the more helpful error message
-    }
-    throw Error(errorMessage);
+	var errorMessage =
+		'Browser does not support some features required by geo-bagging (' +
+		unsupportedFeatures.join(', ') +
+		'), please either use a more modern browser (ideally Chrome) or report a bug at https://github.com/tstibbs/geo-bagging/issues/new.'
+	try {
+		console.log(errorMessage)
+		console.log(typeof showError)
+		if (typeof showError === 'function') {
+			showError(errorMessage)
+		}
+		document.execCommand('Stop')
+	} catch (err) {
+		console.error(err)
+		//catch but just log so we can continue to try to throw the more helpful error message
+	}
+	throw Error(errorMessage)
 }
 
 
@@ -46,8 +49,8 @@ var defaultOptions = {
 $("head").append('<link rel="stylesheet" type="text/css" href="' + window.geoBaggingBaseUrl + 'vendors.css">');
 $("head").append('<link rel="stylesheet" type="text/css" href="' + window.geoBaggingBaseUrl + 'integration~main~mini.css">');
 
-$.ajax($.extend({url: window.geoBaggingBaseUrl + "runtime.b4b924df7bb9d7a8d80b.js"}, defaultOptions));
-$.ajax($.extend({url: window.geoBaggingBaseUrl + "vendors.e97cce86d1dbe52a7e53.js"}, defaultOptions));
-$.ajax($.extend({url: window.geoBaggingBaseUrl + "integration~main~mini.1c92403850335563a8e5.js"}, defaultOptions));
-$.ajax($.extend({url: window.geoBaggingBaseUrl + "integration.d2c5f7e0d0babd45718d.js"}, defaultOptions));
+$.ajax($.extend({url: window.geoBaggingBaseUrl + "runtime.3e4eac708457a510b164.js"}, defaultOptions));
+$.ajax($.extend({url: window.geoBaggingBaseUrl + "vendors.276a02870dea2451e0a0.js"}, defaultOptions));
+$.ajax($.extend({url: window.geoBaggingBaseUrl + "integration~main~mini.ac93b718728e200d3b0e.js"}, defaultOptions));
+$.ajax($.extend({url: window.geoBaggingBaseUrl + "integration.991ec9cc2d1a04397f3e.js"}, defaultOptions));
 
