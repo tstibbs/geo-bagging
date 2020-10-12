b1a3fec3aff98bb69e44bf0ab30e754a72cc000e
Tim Stibbs <tstibbs@users.noreply.github.com>
2020-10-12 20:17:02 +0100

    Updating UI to the production backend deployment

--- ui/src/js/constants.js
+++ ui/src/js/constants.js
@@ -1,5 +1,5 @@
 export default {
 	bingKey: 'AgYJwt3nv3bZyK31EDXorMaO8aIix-5kAa32O5TTwFAhdVYtZdIKhw3ttntsmgqy',
 	dataSources: ['follies', 'hills', 'milestones', 'defence', 'trails', 'rnli', 'nationalparks', 'coastallandmarks'],
-	backendBaseUrl: 'https://europe-west1-geo-bagging-test.cloudfunctions.net/function-1/'
+	backendBaseUrl: 'https://hu01wcuudf.execute-api.eu-west-2.amazonaws.com/default/'
 }
