5e2b28d809669fe62a2bd11203ea25e6c9bb459b
Tim Stibbs <tstibbs@users.noreply.github.com>
2021-01-30 13:47:30 +0000

    Correctly validating cloud-formation scripts

--- backend/test/basic.mjs
+++ backend/test/basic.mjs
@@ -1,11 +1,13 @@
 import {strictEqual} from 'assert'
-import {validate} from '@tstibbs/cloud-core-utils'
+import {validateWithExit} from '@tstibbs/cloud-core-utils'
 
-//check it exposes a function, that's all we can do without proper unit tests
 import dist from '../dist/main.js'
-strictEqual(typeof dist.handler, 'function')
 
 //just check it imports, relatively little value in testing it properly
 import '../deploy/deploy-stack.mjs'
 
-validate('deploy/template.yml')
+//check it exposes a function, that's all we can do without proper unit tests
+strictEqual(typeof dist.handler, 'function')
+
+//validate cf script
+validateWithExit('deploy/template.yml')
