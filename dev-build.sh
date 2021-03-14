3edfaa95445988397043635b0cb3433b52528c39
Tim Stibbs <tstibbs@users.noreply.github.com>
2021-03-14 21:11:44 +0000

    Fixing dev build script

--- dev-build.sh
+++ dev-build.sh
@@ -4,14 +4,15 @@ set -euo pipefail
 
 # this script parses ci.yml in a basic way in order to replicate the build that will be done when it gets pushed.
 
-{ echo "set -euo pipefail"; \
+{
+echo "set -euxo pipefail"; \
 echo "root=\"$(git rev-parse --show-toplevel)\""; \
 grep -E '(run\:|working-directory\:)' .github/workflows/ci.yml | \
 sed 's/run: //' | \
 sed 's/working-directory: /cd /' | \
 awk '{$1=$1;print}' | \
 tr '\n' '%' | \
-sed -E 's#(npm [^%]+)%cd ./([^%]+)(%|$)#cd $root/\2\n\1\n#'g | \
-sed 's#%#\n#' | \
+sed -E 's#(npm [^%]+)%cd geo-bagging(/[^%]+)?(%|$)#cd $root\2\n\1\n#g' | \
+sed 's#%#\n#g' | \
 grep -v 'npm ci';
 } | bash
