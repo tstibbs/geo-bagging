0eb9d5e8d360b297005e09e01818de88a2378ab6
Tim Stibbs <tstibbs@users.noreply.github.com>
2022-10-13 21:07:31 +0100

    Update tests for new backend stack

--- backend/test/assets.test.js
+++ backend/test/assets.test.js
@@ -2,5 +2,5 @@ import {validateCdkAssets} from '@tstibbs/cloud-core-utils'
 import {STACK_NAME} from '../lib/deploy-envs.js'
 
 test('Assets are built as expected', async () => {
-	await validateCdkAssets(STACK_NAME, 1) //should only be the s3 bucket emptier
+	await validateCdkAssets(STACK_NAME, 2) //s3 bucket emptier + lambda api gateway handler
 })
