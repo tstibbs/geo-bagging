11172ec3b1cb1a01c96dcdb4a6102b91ed23b5b8
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-06-27 20:26:08 +0100

    data-prep: using javascript xslt processor to remove the need for Java

--- data-prep/xslt.js
+++ data-prep/xslt.js
@@ -1,40 +1,11 @@
 import {spawn} from 'child_process'
-import fs from 'fs'
-import {download as downloadFiles} from './downloader.js'
 import {tmpInputDir} from './constants.js'
 
-const artifact = `Saxon-HE`
-const version = `10.9`
-const jarName = `${artifact}-${version}.jar`
-const downloadPath = `https://repo1.maven.org/maven2/net/sf/saxon/${artifact}/${version}/${jarName}`
-const jarPath = `${tmpInputDir}/${jarName}`
-
-let downloadInProgress = null
-
-function _downloadJar() {
-	if (downloadInProgress == null) {
-		if (fs.existsSync(jarPath)) {
-			downloadInProgress = Promise.resolve()
-		} else {
-			downloadInProgress = downloadFiles(null, {[downloadPath]: jarName})
-		}
-	}
-	return downloadInProgress
-}
-
 async function transform(xslt, input, output) {
-	await _downloadJar()
 	return new Promise((resolve, reject) => {
-		//assumes java is on the path
 		let child = spawn(
-			`java`,
-			[
-				`-jar`,
-				`${tmpInputDir}/${jarName}`,
-				`-xsl:${xslt}`,
-				`-s:${tmpInputDir}/${input}`,
-				`-o:${tmpInputDir}/${output}`
-			],
+			`npx`,
+			[`xslt3`, `-xsl:${xslt}`, `-s:${tmpInputDir}/${input}`, `-o:${tmpInputDir}/${output}`, `-t`],
 			{
 				stdio: 'inherit'
 			}
