4351990b650701bd333164c79a21dd67233bea2f
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-07-07 19:59:04 +0100

    data-prep: adding retries to downloads

--- data-prep/downloader.js
+++ data-prep/downloader.js
@@ -1,9 +1,10 @@
 import fs from 'fs'
 import request from 'request'
+import {backOff} from 'exponential-backoff'
 import {tmpInputDir} from './constants.js'
 import {createTempDir} from './utils.js'
 
-function _downloadSingle(source, destination) {
+async function _httpDownload(source, destination) {
 	return new Promise((resolve, reject) => {
 		let file = fs.createWriteStream(destination)
 		let req = request(source)
@@ -37,6 +38,13 @@ function _downloadSingle(source, destination) {
 	})
 }
 
+async function _downloadSingle(source, destination, downloaderFunction) {
+	return await backOff(() => downloaderFunction(source, destination), {
+		numOfAttempts: 3,
+		startingDelay: 1000 //one second
+	})
+}
+
 export async function _downloadMultiple(bundleName, urls, downloaderFunction) {
 	let outputDir = `${tmpInputDir}`
 	if (bundleName != null) {
@@ -48,7 +56,7 @@ export async function _downloadMultiple(bundleName, urls, downloaderFunction) {
 	try {
 		for (const [url, fileName] of orderedArrayOfUrls) {
 			console.log(`Downloading ${url} to ${outputDir}/${fileName}`)
-			await downloaderFunction(url, outputDir + '/' + fileName)
+			await _downloadSingle(url, outputDir + '/' + fileName, downloaderFunction)
 		}
 	} catch (err) {
 		console.log(`Error downloading '${from}': ${err}`)
@@ -57,5 +65,5 @@ export async function _downloadMultiple(bundleName, urls, downloaderFunction) {
 }
 
 export async function download(bundleName, urls) {
-	return await _downloadMultiple(bundleName, urls, _downloadSingle)
+	return await _downloadMultiple(bundleName, urls, _httpDownload)
 }
