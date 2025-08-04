5ead2cad96e9a0f5fc1331c9c9bcf16015351027
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-08-04 21:14:33 +0100

    data-prep: creating a generic geojson source to simplify new sources

---
+++ data-prep/generic-source-geojson.js
@@ -0,0 +1,25 @@
+import {readFile} from 'fs/promises'
+
+import Converter from './converter.js'
+import {tmpInputDir, outputDir} from './constants.js'
+import {visualise as visualiseGeoJson, compare as compareGeoJson} from './geojson-comparer.js'
+import {backUpReferenceData, writeFile} from './utils.js'
+
+export async function readDownloadedFiles(sourceName, files) {
+	const inputDirectory = `${tmpInputDir}/${sourceName}`
+	return Promise.all(files.map(file => `${inputDirectory}/${file}`).map(file => readFile(file)))
+}
+
+export async function process(sourceName, dataProducer) {
+	await backUpReferenceData(sourceName, 'data.geojson')
+	let data = await dataProducer()
+	const fileName = `${outputDir}/${sourceName}/data.geojson`
+	await writeFile(fileName, JSON.stringify(data), 'utf-8')
+
+	const converter = new Converter()
+	let lastUpdated = converter.getLastUpdatedString()
+	await converter.writeMetaData(fileName, data.features.length, lastUpdated)
+	await visualiseGeoJson(sourceName, 'old')
+	await visualiseGeoJson(sourceName, 'new')
+	return await compareGeoJson(sourceName)
+}
--- data-prep/nationalparks.js
+++ data-prep/nationalparks.js
@@ -1,35 +1,26 @@
-import Converter from './converter.js'
-import {tmpInputDir, outputDir} from './constants.js'
-import {visualise as visualiseGeoJson, compare as compareGeoJson} from './geojson-comparer.js'
 import {ifCmd} from '@tstibbs/cloud-core-utils'
-import {backUpReferenceData, readFile, writeFile} from './utils.js'
+import {process, readDownloadedFiles} from './generic-source-geojson.js'
 
-const inputDirectory = `${tmpInputDir}/nationalparks`
+const sourceName = 'nationalparks'
 
 async function buildDataFile() {
-	await backUpReferenceData('nationalparks', 'data.geojson')
-	let contents = await readFile(`${inputDirectory}/NationalParks.geojson`)
-	let data = JSON.parse(contents)
-	data.features = data.features.map(feature => {
-		let name = feature.properties['NPARK21NM']
-		let match = /^(The )?(.*?)( National Park| Authority)?$/g.exec(name)
-		if (match != null && match[2] != null) {
-			name = match[2]
-		}
-		feature.properties = {
-			name: name
-		}
-		return feature
-	})
-	const fileName = `${outputDir}/nationalparks/data.geojson`
-	await writeFile(fileName, JSON.stringify(data), 'utf-8')
-
-	const converter = new Converter()
-	let lastUpdated = converter.getLastUpdatedString()
-	await converter.writeMetaData(fileName, data.features.length, lastUpdated)
-	await visualiseGeoJson('nationalparks', 'old')
-	await visualiseGeoJson('nationalparks', 'new')
-	return await compareGeoJson('nationalparks')
+	const dataProducer = async () => {
+		let [contents] = await readDownloadedFiles(sourceName, ['NationalParks.geojson'])
+		let data = JSON.parse(contents)
+		data.features = data.features.map(feature => {
+			let name = feature.properties['NPARK21NM']
+			let match = /^(The )?(.*?)( National Park| Authority)?$/g.exec(name)
+			if (match != null && match[2] != null) {
+				name = match[2]
+			}
+			feature.properties = {
+				name: name
+			}
+			return feature
+		})
+		return data
+	}
+	return await process(sourceName, dataProducer)
 }
 
 await ifCmd(import.meta, buildDataFile)
