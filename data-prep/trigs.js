280d9f50b66c045a7393b99229838621f5c59c5d
Tim Stibbs <tstibbs@users.noreply.github.com>
2025-09-25 19:59:42 +0100

    data-prep: overriding location of a few incorrect trig points

--- data-prep/trigs.js
+++ data-prep/trigs.js
@@ -7,6 +7,14 @@ import compareData from './csv-comparer.js'
 const attributionString = `Adapted from data available on <a href="https://trigpointing.uk/">trigpointing.uk</a> which is a mixture of Public Domain and OGL from Ordnance Survey.`
 const columnHeaders = '[Longitude,Latitude,Id,Name,physical_type,condition]'
 
+//locations are incorrect for a few points, so we add overrides to correct them
+const locationOverrides = {
+	TP25776: [-2.2298, 49.709],
+	TP25773: [-2.19806, 49.71036],
+	TP25777: [-2.1781, 49.7167],
+	TP25774: [-2.22416, 49.7032]
+}
+
 class TrigConverter extends Converter {
 	constructor(filter) {
 		super(attributionString, columnHeaders)
@@ -15,13 +23,19 @@ class TrigConverter extends Converter {
 
 	extractColumns(record) {
 		if (record.length > 1) {
-			let lngLat = this._convertGridRef(record[0])
+			let id = record[1]
+			let lngLat
+			if (id in locationOverrides) {
+				lngLat = locationOverrides[id]
+			} else {
+				lngLat = this._convertGridRef(record[0])
+			}
 			let condition = record[7]
 			if (lngLat != null && (this._filter == null || this._filter(lngLat, condition) === true)) {
 				return [
 					lngLat[0], //lng
 					lngLat[1], //lat
-					record[1], //waypoint (i.e. id)
+					id, //waypoint (i.e. id)
 					record[2], //name
 					record[4], //physical_type
 					condition
