1651799a4eff27162f2f80a0a3c96040f12859d0
Tim Stibbs <tstibbs@users.noreply.github.com>
2026-06-24 20:08:07 +0000

    backend: removing legacy test that's no longer relevant

--- backend/test/assets.test.js
+++
@@ -1,6 +0,0 @@
-import {validateCdkAssets} from '@tstibbs/cloud-core-utils'
-import {STACK_NAME} from '../lib/deploy-envs.js'
-
-test('Assets are built as expected', async () => {
-	await validateCdkAssets(STACK_NAME, 2) //s3 bucket emptier + lambda api gateway handler
-})
